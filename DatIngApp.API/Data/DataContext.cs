ae4a55a05b41fc283384df8a7d92811398642938
Ezequiel Rivas <33492016+angles11@users.noreply.github.com>
2020-05-07 13:01:08 -0300

    Initial Commit

---
+++ DatIngApp.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using DatIngApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatIngApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly DbContextOptions<DataContext> _options;
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            _options = options;
+        }
+
+        public DbSet<Value> Values{ get; set; }
+    }
+}
